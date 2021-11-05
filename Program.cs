2121b3e494ce30400b90c4ba942f6f704dc17c34
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-11-05 13:42:26 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace vadsomhelst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testar lite");
         }
     }
 }
